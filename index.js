36696f215a0b7a8edf9a1844d11de045d32d1322
Dheeraj Pali <dheerajpali761@gmail.com>
2025-02-24 18:55:27 +0530

    change in tocken type

--- index.js
+++ index.js
@@ -12,16 +12,25 @@ app.use(bodyParser.json());
 
 // Verification endpoint
 app.get("/webhook", (req, res) => {
-  const verificationToken = EAAWTGSiIZBiQBO8f993oXvNBQfqlV3yRZBTGmobWVEEQc6S6agxrIisJYNqLP7nxsbBq0MowU7x1jF7DXU2fUJl7nFXtZBnwiEeHgv7gNKMcny2uK4IsK3iz4ZBijOBZCmOe3j47FsK6aRBo2OOy2Fw3dlp8E5unF73xVvM5byTQMrReohI2ybsgP6to8YdDkhV5ZBZB1lACmKbVWcagHhITZC6w74gZD;
-  const mode = req.query["hub.mode"];
-  const token = req.query["hub.verification_token"];
-  const challenge = req.query["hub.challenge"];
-
-  if (mode && token === verificationToken) {
-    console.log("Webhook verified!");
-    res.status(200).send(challenge);
-  } else {
-    res.sendStatus(403);
+    console.log("inside get");
+  try {
+    const verificationToken =
+      "EAAWTGSiIZBiQBO8f993oXvNBQfqlV3yRZBTGmobWVEEQc6S6agxrIisJYNqLP7nxsbBq0MowU7x1jF7DXU2fUJl7nFXtZBnwiEeHgv7gNKMcny2uK4IsK3iz4ZBijOBZCmOe3j47FsK6aRBo2OOy2Fw3dlp8E5unF73xVvM5byTQMrReohI2ybsgP6to8YdDkhV5ZBZB1lACmKbVWcagHhITZC6w74gZD";
+    const mode = req.query["hub.mode"];
+    const token = req.query["hub.verification_token"];
+    const challenge = req.query["hub.challenge"];
+    console.log(token);
+    console.log(verificationToken);
+    // console.log(token == verificationToken);
+    console.log(mode);
+    if (mode && token == verificationToken) {
+      console.log("Webhook verified!");
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  } catch (error) {
+    console.log("error occured", error);
   }
 });
 
