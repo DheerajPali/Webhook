b9187c7c8a35d9a16e3936d74d638778920c91dd
Dheeraj Pali <dheerajpali761@gmail.com>
2025-02-25 20:30:47 +0530

    added semicolon

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cors());
