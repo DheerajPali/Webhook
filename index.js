2251500500820c00f517d998472086014863522d
Dheeraj Pali <dheerajpali761@gmail.com>
2025-03-10 19:48:41 +0530

    solved cors errors

--- index.js
+++ index.js
@@ -11,16 +11,16 @@ const port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(cors({
-  origin: ["http://localhost:5173"], 
-  methods: ["GET", "POST"]
+  origin: "*", 
+  methods: "*"
 }));
 
 // Create HTTP Server
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
-    methods: ["GET", "POST"]
+    origin: "*", 
+  methods: "*"
   }
 });
 
